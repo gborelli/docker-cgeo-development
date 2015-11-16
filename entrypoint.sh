0c78cc357ebb000192577bd8e5c48d482d5343f4
Giorgio Borelli <giorgio.borelli@abstract.it>
2015-11-16 15:55:47 +0100

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 args=("$@")
 
 # Compile egg-info directory for each package in src directory
-if [ ! -d ./src ]; then
+if [ -d ./src ]; then
     for pkj in src/*; do ( cd $pkj && if [ ! -d $pkj.egg-info ]; then python setup.py egg_info; fi  ) done
 fi
 
